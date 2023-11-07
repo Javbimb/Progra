ea77c2de7d90cbc5f8a8bd4875545f968b06aef6
Javbimb <143531224+Javbimb@users.noreply.github.com>
2023-11-07 10:15:01 -0600

    Add files via upload

---
+++ semana 14/semana 14.py
@@ -0,0 +1,39 @@
+class Automovil:
+    def __init__(self):
+        self.modelo = 0
+        self.precio = 0.0
+        self.marca = ""
+        self.disponible = True
+        self.tipoCambioDolar = 0.0
+        self.descuentoAplicado = 0.0
+
+    def DefinirModelo(self, unModelo):
+        self.modelo = unModelo
+
+    def DefinirPrecio(self, unPrecio):
+        self.precio = unPrecio
+
+    def DefinirMarca(self, unaMarca):
+        self.marca = unaMarca
+
+    def DefinirTipoCambio(self, unTipoCambio):
+        self.tipoCambioDolar = unTipoCambio
+
+    def CambiarDisponibilidad(self):
+        self.disponible = not self.disponible
+
+    def MostrarDisponibilidad(self):
+        if self.disponible:
+            return "Disponible"
+        else:
+            return "No se encuentra disponible actualmente"
+
+    def MostrarInformacion(self):
+        precio_en_dolares = self.precio / self.tipoCambioDolar
+        return f"Marca: {self.marca}. Modelo: {self.modelo}. Precio de venta: Q{self.precio}. Precio en dólares ${precio_en_dolares}. {self.MostrarDisponibilidad()}"
+
+    def AplicarDescuento(self, miDescuento):
+        self.descuentoAplicado = miDescuento
+        self.precio -= self.descuentoAplicado
+        self.DefinirPrecio(self.precio)
+
